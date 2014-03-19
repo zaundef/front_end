52279f467933358c5e844a45d855f943c1161983
Vincent ELLAMA <vincent.ellama@gmail.com>
2014-03-19 09:43:33 +0100

    first_commit

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+	require('load-grunt-tasks')(grunt);
+
+	var gruntTasksPath = './grunt_tasks';
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		autoprefixer: require(gruntTasksPath+'/autoprefixer'),
+		clean: require(gruntTasksPath+'/clean'),
+		cssmin: require(gruntTasksPath+'/cssmin'),
+		compass: require(gruntTasksPath+'/compass'),
+		connect: require(gruntTasksPath+'/connect'),
+		copy: require(gruntTasksPath+'/copy'),
+		mkdir: require(gruntTasksPath+'/mkdir'),
+		jade: require(gruntTasksPath+'/jade'),
+		stylus: require(gruntTasksPath+'/stylus'),
+		uglify: require(gruntTasksPath+'/uglify'),
+		watch: require(gruntTasksPath+'/watch')
+	});
+
+	grunt.registerTask('init', ['mkdir:base']);
+
+	grunt.registerTask('default', ['clean', 'copy', 'uglify', 'jade', 'stylus', 'autoprefixer', 'cssmin', 'connect', 'watch']);
+};
